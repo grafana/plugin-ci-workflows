9eedf813df9fdd7cfd50848fd09df1b4fb683f42
Giuseppe Guerra <giuseppe.guerra@grafana.com>
2025-12-12 13:01:52 +0100

    test(act): mock steps and jobs, add artifacts interface, add more tests (#442)

---
+++ tests/act/internal/workflow/ci.go
@@ -0,0 +1,162 @@
+package workflow
+
+import (
+	"fmt"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	pciwfBaseRef = "grafana/plugin-ci-workflows/.github/workflows"
+)
+
+// SimpleCI is a predefined GitHub Actions workflow for testing plugins using act.
+// It uses the plugin-ci-workflows CI workflow as a base, with sane default values
+// and allows customization through options.
+// It implements the Marshalable interface to allow conversion to YAML format.
+// Instances must be created using NewSimpleCI.
+type SimpleCI struct {
+	*TestingWorkflow
+}
+
+// NewSimpleCI creates a new SimpleCI workflow instance with default settings.
+// The caller can provide options to customize the workflow.
+func NewSimpleCI(opts ...SimpleCIOption) (SimpleCI, error) {
+	ciBaseWf := BaseWorkflow{
+		Name: "CI",
+		On: On{
+			Push: OnPush{
+				Branches: []string{"main"},
+			},
+			PullRequest: OnPullRequest{
+				Branches: []string{"main"},
+			},
+		},
+		Jobs: map[string]*Job{
+			"ci": {
+				Name: "CI",
+				// This will be populated later with the child testing workflow reference
+				// Uses: "..."
+				Permissions: Permissions{
+					"contents": "read",
+					"id-token": "write",
+				},
+				With: map[string]any{
+					"plugin-version-suffix": "${{ github.event_name == 'pull_request' && github.event.pull_request.head.sha || '' }}",
+					"testing":               true,
+				},
+				Secrets: Secrets{
+					"GITHUB_TOKEN": "${{ secrets.GITHUB_TOKEN }}",
+				},
+			},
+		},
+	}
+
+	// We need to create a child testing workflow for the called "ci.yml" workflow, in order to mock jobs/steps in it
+	// Read the base workflow from file to create the child BaseWorkflow
+	childBaseWf, err := NewBaseWorkflowFromFile(filepath.Join(".github", "workflows", "ci.yml"))
+	if err != nil {
+		return SimpleCI{}, fmt.Errorf("new base workflow from file for child ci workflow: %w", err)
+	}
+
+	// Workflow to be returned
+	testingWf := SimpleCI{NewTestingWorkflow("simple-ci", ciBaseWf)}
+
+	// Add the child workflow (ci) now, so further customization can be done on it via opts.
+	// Use the same UUID as the parent, so they have the same uuid in the file name
+	// and it is easier to correlate them.
+	childTestingWf := NewTestingWorkflow("ci", childBaseWf, withUUID(testingWf.uuid))
+	testingWf.AddChild("ci", childTestingWf)
+
+	// Change the parent workflow so it calls the mocked child workflow
+	testingWf.BaseWorkflow.Jobs["ci"].Uses = pciwfBaseRef + "/" + testingWf.GetChild("ci").FileName() + "@main"
+
+	// Add uuid to each job in the workflow and all its children in order to
+	// make unique contianer names and allow tests to run in parallel, so that
+	// container names created by act don't clash
+	// TODO: move to TestingWorkflow instead?
+	for _, wf := range append([]Workflow{testingWf.TestingWorkflow}, testingWf.Children()...) {
+		for _, j := range wf.Jobs() {
+			if j.Name != "" {
+				j.Name = j.Name + "-" + testingWf.uuid.String()
+			} else {
+				j.Name = testingWf.uuid.String()
+			}
+		}
+	}
+
+	// Apply options to customize the SimpleCI instance.
+	// These opts can also modify the child testing workflow.
+	for _, opt := range opts {
+		opt(&testingWf)
+	}
+	return testingWf, nil
+}
+
+// CIWorkflow returns the TestingWorkflow instance representing the "ci" child workflow.
+// This can be used to further customize/mock steps and jobs in the child workflow.
+func (w *SimpleCI) CIWorkflow() *TestingWorkflow {
+	return w.GetChild("ci")
+}
+
+// SimpleCIOption is a function that modifies a SimpleCI instance during its construction.
+type SimpleCIOption func(*SimpleCI)
+
+// WithPluginDirectoryInput sets the plugin-directory input for the CI job in the SimpleCI workflow.
+func WithPluginDirectoryInput(dir string) SimpleCIOption {
+	return func(w *SimpleCI) {
+		w.BaseWorkflow.Jobs["ci"].With["plugin-directory"] = dir
+	}
+}
+
+// WithDistArtifactPrefixInput sets the dist-artifacts-prefix input for the CI job in the SimpleCI workflow.
+func WithDistArtifactPrefixInput(prefix string) SimpleCIOption {
+	return func(w *SimpleCI) {
+		w.BaseWorkflow.Jobs["ci"].With["dist-artifacts-prefix"] = prefix
+	}
+}
+
+// WithPlaywrightInput sets the run-playwright input for the CI job in the SimpleCI workflow.
+func WithPlaywrightInput(enabled bool) SimpleCIOption {
+	return func(w *SimpleCI) {
+		w.BaseWorkflow.Jobs["ci"].With["run-playwright"] = enabled
+	}
+}
+
+// WithRunPluginValidatorInput sets the run-plugin-validator input for the CI job in the SimpleCI workflow.
+func WithRunPluginValidatorInput(enabled bool) SimpleCIOption {
+	return func(w *SimpleCI) {
+		w.BaseWorkflow.Jobs["ci"].With["run-plugin-validator"] = enabled
+	}
+}
+
+// WithRunTruffleHogInput sets the run-trufflehog input for the CI job in the SimpleCI workflow.
+func WithRunTruffleHogInput(enabled bool) SimpleCIOption {
+	return func(w *SimpleCI) {
+		w.BaseWorkflow.Jobs["ci"].With["run-trufflehog"] = enabled
+	}
+}
+
+// WithMockedDist modifies the SimpleCI workflow to mock the test-and-build job
+// to copy pre-built dist files (js + assets + backend executable, NOT the ZIP files)
+// from the tests/act/mockdata folder instead of building them.
+// This can be used for tests that need to assert on side-effects of building the plugin,
+// without actually building it, which saves execution time.
+// The pluginFolder parameter is the name of the plugin folder inside tests/act/mockdata/dist.
+func WithMockedDist(t *testing.T, pluginFolder string) SimpleCIOption {
+	return func(w *SimpleCI) {
+		testAndBuild := w.CIWorkflow().BaseWorkflow.Jobs["test-and-build"]
+		// require.NoError(t, testAndBuild.RemoveStep("setup"))
+		require.NoError(t, testAndBuild.ReplaceStep(
+			"frontend",
+			CopyMockFilesStep("dist/"+pluginFolder, "${{ github.workspace }}/${{ inputs.plugin-directory }}/dist/"),
+		))
+		require.NoError(t, testAndBuild.RemoveStep("backend"))
+	}
+}
+
+// Static checks
+
+var _ Workflow = SimpleCI{}
--- tests/act/internal/workflow/workflow.go
+++ tests/act/internal/workflow/workflow.go
@@ -2,67 +2,219 @@
 // It provides a way to programmatically create workflows and jobs in a structured and type-safe manner.
 package workflow
 
-import "github.com/goccy/go-yaml"
+import (
+	"fmt"
+	"os"
+	"strings"
 
-// Marshalable is an interface for workflows that can be marshaled to YAML format.
-type Marshalable interface {
+	"github.com/goccy/go-yaml"
+)
+
+// Workflow is an interface for workflows that can be marshaled to YAML format.
+type Workflow interface {
+	// FileName returns the file name for the workflow.
+	FileName() string
+
+	// Marshal converts the Workflow instance to its YAML representation.
 	Marshal() ([]byte, error)
+
+	Children() []Workflow
+	Jobs() map[string]*Job
 }
 
-// Workflow represents a GitHub Actions workflow definition.
-// It implements the Marshalable interface to allow conversion to YAML format.
-type Workflow struct {
+// BaseWorkflow represents a GitHub Actions workflow definition.
+type BaseWorkflow struct {
 	Name        string
 	On          On
-	Permissions Permissions
+	Permissions Permissions       `yaml:"permissions,omitempty"`
+	Env         map[string]string `yaml:"env,omitempty"`
+	Jobs        map[string]*Job
+}
 
-	// Jobs is a map slice of job names to Job definitions.
-	// It uses yaml.MapSlice to preserve the order of jobs as defined.
-	// Example:
-	//
-	// ```go
-	// 	yaml.MapSlice{
-	//     	yaml.MapItem{Key: "build", Value: Job{...}},
-	//     	yaml.MapItem{Key: "test", Value: Job{...}},
-	// 	}
-	// ```
-	Jobs yaml.MapSlice
+// NewBaseWorkflowFromFile creates a BaseWorkflow instance by reading and parsing a YAML file at the given path.
+func NewBaseWorkflowFromFile(path string) (BaseWorkflow, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return BaseWorkflow{}, fmt.Errorf("open workflow file: %w", err)
+	}
+	defer f.Close()
+	var bw BaseWorkflow
+	if err := yaml.NewDecoder(f).Decode(&bw); err != nil {
+		return BaseWorkflow{}, fmt.Errorf("decode workflow file: %w", err)
+	}
+	return bw, nil
 }
 
 // Marshal converts the Workflow instance to its YAML representation.
-func (w Workflow) Marshal() ([]byte, error) {
+func (w *BaseWorkflow) Marshal() ([]byte, error) {
 	return yaml.Marshal(w)
 }
 
-// FirstJob returns a pointer to the first job in the workflow.
-// If there are no jobs, it returns empty string and nil.
-func (w Workflow) FirstJob() *Job {
-	for _, job := range w.Jobs {
-		return job.Value.(*Job)
+// Permissions is the YAML representation of GitHub Actions job permissions.
+type Permissions map[string]string
+
+// Secrets is the YAML representation of GitHub Actions job secrets.
+type Secrets map[string]string
+
+// Steps is the YAML representation of a list of GitHub Actions steps.
+type Steps []Step
+
+// Job is the YAML representation of a GitHub Actions job.
+type Job struct {
+	Name string `yaml:"name,omitempty"`
+
+	If string `yaml:"if,omitempty"`
+
+	RunsOn  string            `yaml:"runs-on,omitempty"`
+	Needs   []string          `yaml:"needs,omitempty"`
+	Outputs map[string]string `yaml:"outputs,omitempty"`
+
+	Permissions Permissions `yaml:"permissions,omitempty"`
+
+	Uses string         `yaml:"uses,omitempty"`
+	With map[string]any `yaml:"with,omitempty"`
+
+	Secrets Secrets `yaml:"secrets,omitempty"`
+
+	Steps Steps `yaml:"steps,omitempty"`
+
+	Container ContainerJob `yaml:"container,omitempty"`
+}
+
+// ReplaceStep replaces (mocks) a step with the given id with the provided steps.
+// The target step is replaced in place by the new steps.
+// If more than one step is provided, they will be injected at the same position as the original step
+// in place of the original step.
+// This can be used for mocking steps in tests.
+func (j *Job) ReplaceStep(id string, steps ...Step) error {
+	stepIndex := j.getStepIndex(id)
+	if stepIndex == -1 {
+		return fmt.Errorf("step with id %q not found", id)
+	}
+	// Replace the step with the new steps, injecting them at the same position
+	j.Steps = append(j.Steps[:stepIndex], append(steps, j.Steps[stepIndex+1:]...)...)
+	return nil
+}
+
+// RemoveStep removes a step with the given id from the job's steps.
+// If the step is not found, an error is returned.
+// This can be used for removing steps in tests, for example to skip certain actions
+// that are not relevant to the test in order to speed up execution.
+// Be careful when removing steps that are required by other steps (e.g.: steps that set outputs
+// used by later steps), as this may cause the workflow to fail.
+func (j *Job) RemoveStep(id string) error {
+	stepIndex := j.getStepIndex(id)
+	if stepIndex == -1 {
+		return fmt.Errorf("step with id %q not found", id)
+	}
+	// Remove the step
+	j.Steps = append(j.Steps[:stepIndex], j.Steps[stepIndex+1:]...)
+	return nil
+}
+
+// getStepIndex returns the index of the step with the given id.
+// If the step is not found, -1 is returned.
+func (j *Job) getStepIndex(id string) int {
+	for i, step := range j.Steps {
+		if step.ID == id {
+			return i
+		}
+	}
+	return -1
+}
+
+// GetStep retrieves a step with the given id from the job's steps.
+// If the step is not found, nil is returned.
+func (j *Job) GetStep(id string) *Step {
+	for i, step := range j.Steps {
+		if step.ID == id {
+			return &j.Steps[i]
+		}
 	}
 	return nil
 }
 
-type Permissions map[string]string
-type Secrets map[string]string
-
-type Job struct {
-	Name        string
-	Uses        string
-	Permissions Permissions
-	With        map[string]any
-	Secrets     Secrets
+// ContainerJob is the YAML representation of a GitHub Actions job running in a container.
+type ContainerJob struct {
+	Image   string   `yaml:"image,omitempty"`
+	Volumes []string `yaml:"volumes,omitempty"`
 }
 
+// Step is the YAML representation of a GitHub Actions step.
+type Step struct {
+	Name string `yaml:"name,omitempty"`
+	ID   string `yaml:"id,omitempty"`
+
+	If string `yaml:"if,omitempty"`
+
+	Uses string         `yaml:"uses,omitempty"`
+	With map[string]any `yaml:"with,omitempty"`
+
+	Run              string `yaml:"run,omitempty"`
+	Shell            string `yaml:"shell,omitempty"`
+	WorkingDirectory string `yaml:"working-directory,omitempty"`
+
+	Env map[string]string `yaml:"env,omitempty"`
+}
+
+// On is the YAML representation of GitHub Actions workflow triggers.
 type On struct {
-	Push        OnPush        `yaml:"push,omitempty"`
-	PullRequest OnPullRequest `yaml:"pull_request,omitempty"`
+	Push         OnPush         `yaml:"push,omitempty"`
+	PullRequest  OnPullRequest  `yaml:"pull_request,omitempty"`
+	WorkflowCall OnWorkflowCall `yaml:"workflow_call,omitempty"`
 }
 
+// OnPush is the YAML representation of GitHub Actions push event trigger.
 type OnPush struct {
 	Branches []string `yaml:"branches,omitempty"`
 }
 
+// OnPullRequest is the YAML representation of GitHub Actions pull_request event trigger.
 type OnPullRequest struct {
 	Branches []string `yaml:"branches,omitempty"`
+}
+
+// OnWorkflowCall is the YAML representation of GitHub Actions workflow_call event trigger.
+type OnWorkflowCall struct {
+	Inputs  map[string]WorkflowCallInput  `yaml:"inputs,omitempty"`
+	Outputs map[string]WorkflowCallOutput `yaml:"outputs,omitempty"`
+}
+
+// WorkflowCallInput is the YAML representation of a GitHub Actions workflow call input field.
+type WorkflowCallInput struct {
+	Description string `yaml:"description,omitempty"`
+	Type        string `yaml:"type,omitempty"`
+	Required    bool   `yaml:"required,omitempty"`
+	Default     any    `yaml:"default,omitempty"`
+}
+
+// WorkflowCallOutput is the YAML representation of a GitHub Actions workflow call output field.
+type WorkflowCallOutput struct {
+	Description string `yaml:"description,omitempty"`
+	Value       string `yaml:"value,omitempty"`
+}
+
+// Commands is a utility type that represents a list of shell commands.
+// It provides a String method to join the commands into a single string
+// separated by newlines, suitable for use in a Step's Run field.
+// This is handy for defining multi-line shell scripts without having to manually concat strings.
+// Example:
+//
+// ```go
+//
+//	step := Step{
+//		Name: "Example Step",
+//		Run:  Commands{
+//			"echo Hello, World!",
+//			"ls -la"
+//		}.String(),
+//		Shell: "bash",
+//	}
+//
+// ```
+type Commands []string
+
+// String joins the commands into a single string separated by newlines.
+func (c Commands) String() string {
+	return strings.Join(c, "\n")
 }
