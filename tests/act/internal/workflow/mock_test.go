792561a2e14f02d20b235fe445b635a52d9d01f0
Giuseppe Guerra <giuseppe@guerra.in>
2026-01-15 14:15:18 +0100

    add unit tests for new mocks

---
+++ tests/act/internal/workflow/mock_test.go
@@ -0,0 +1,139 @@
+package workflow
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMockVaultSecretsStep(t *testing.T) {
+	const (
+		vaultAction = "grafana/shared-workflows/actions/get-vault-secrets@a37de51f3d713a30a9e4b21bcdfbd38170020593"
+		bashOutput  = `echo "secrets=${SECRETS_JSON}" >> "$GITHUB_OUTPUT"`
+	)
+	vault := VaultSecrets{
+		CommonSecrets: map[string]string{
+			"common_secret_1:a": "value1",
+			"common_secret_2:b": "value2",
+		},
+		RepoSecrets: map[string]string{
+			"repo_secret_1:c": "value3",
+			"repo_secret_2:d": "value4",
+		},
+	}
+
+	t.Run("common secrets", func(t *testing.T) {
+		step := Step{
+			Name: "Get Vault Secrets",
+			Uses: vaultAction,
+			With: map[string]any{
+				"common_secrets": strings.Join([]string{
+					"SECRET1=common_secret_1:a",
+					"SECRET2=common_secret_2:b",
+				}, "\n"),
+				"export_env": false,
+			},
+		}
+		mockedStep, err := MockVaultSecretsStep(step, vault)
+		require.NoError(t, err)
+		require.Equal(t, "Get Vault Secrets (mocked)", mockedStep.Name)
+		require.Contains(t, mockedStep.Run, bashOutput)
+		exp := `{"SECRET1":"value1","SECRET2":"value2"}`
+		require.Equal(t, exp, mockedStep.Env["SECRETS_JSON"])
+	})
+
+	t.Run("repo secrets", func(t *testing.T) {
+		step := Step{
+			Name: "Get Vault Secrets",
+			Uses: vaultAction,
+			With: map[string]any{
+				"repo_secrets": strings.Join([]string{
+					"C=repo_secret_1:c",
+					"D=repo_secret_2:d",
+				}, "\n"),
+				"export_env": false,
+			},
+		}
+		mockedStep, err := MockVaultSecretsStep(step, vault)
+		require.NoError(t, err)
+		require.Equal(t, "Get Vault Secrets (mocked)", mockedStep.Name)
+		require.Contains(t, mockedStep.Run, bashOutput)
+		exp := `{"C":"value3","D":"value4"}`
+		require.Equal(t, exp, mockedStep.Env["SECRETS_JSON"])
+	})
+
+	t.Run("common + repo secrets", func(t *testing.T) {
+		step := Step{
+			Name: "Get Vault Secrets",
+			Uses: vaultAction,
+			With: map[string]any{
+				"common_secrets": strings.Join([]string{
+					"SECRET1=common_secret_1:a",
+					"SECRET2=common_secret_2:b",
+				}, "\n"),
+				"repo_secrets": strings.Join([]string{
+					"C=repo_secret_1:c",
+					"D=repo_secret_2:d",
+				}, "\n"),
+				"export_env": false,
+			},
+		}
+		mockedStep, err := MockVaultSecretsStep(step, vault)
+		require.NoError(t, err)
+		require.Equal(t, "Get Vault Secrets (mocked)", mockedStep.Name)
+		require.Contains(t, mockedStep.Run, bashOutput)
+		exp := `{"C":"value3","D":"value4","SECRET1":"value1","SECRET2":"value2"}`
+		require.Equal(t, exp, mockedStep.Env["SECRETS_JSON"])
+	})
+
+	t.Run("unexisting secret", func(t *testing.T) {
+		step := Step{
+			Name: "Get Vault Secrets",
+			Uses: vaultAction,
+			With: map[string]any{
+				"common_secrets": strings.Join([]string{
+					"SECRET1=this_secret_does_not_exist:a",
+				}, "\n"),
+				"export_env": false,
+			},
+		}
+		_, err := MockVaultSecretsStep(step, vault)
+		require.ErrorContains(t, err, "this_secret_does_not_exist")
+		require.ErrorContains(t, err, "not found in provided fake secrets")
+	})
+
+	t.Run("unexisting secret with default value", func(t *testing.T) {
+		step := Step{
+			Name: "Get Vault Secrets",
+			Uses: vaultAction,
+			With: map[string]any{
+				"common_secrets": strings.Join([]string{
+					"SECRET1=a:b",
+					"SECRET2=this_secret_does_not_exist:a",
+				}, "\n"),
+				"export_env": false,
+			},
+		}
+		defaultValue := "foo"
+		mockedStep, err := MockVaultSecretsStep(step, VaultSecrets{
+			CommonSecrets: map[string]string{
+				"a:b": "c",
+			},
+			DefaultValue: &defaultValue,
+		})
+		require.NoError(t, err)
+		require.Equal(t, `{"SECRET1":"c","SECRET2":"foo"}`, mockedStep.Env["SECRETS_JSON"])
+	})
+}
+
+func TestMockArgoWorkflowStep(t *testing.T) {
+	step := Step{
+		Name: "Trigger Argo Workflow",
+		Uses: "grafana/shared-workflows/actions/trigger-argo-workflow@e100806688f1209051080dfea5719fbbd1d18cc0",
+	}
+	mockedStep, err := MockArgoWorkflowStep(step)
+	require.NoError(t, err)
+	require.Equal(t, "Trigger Argo Workflow (mocked)", mockedStep.Name)
+	require.Contains(t, mockedStep.Run, `echo "uri=https://mock-argo-workflows.example.com/workflows/grafana-plugins-cd/mock-workflow-id" >> "$GITHUB_OUTPUT"`)
+}
