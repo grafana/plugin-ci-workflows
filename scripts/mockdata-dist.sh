159085d06557eee1b551bcc2335c89968d57c850
Giuseppe Guerra <giuseppe@guerra.in>
2026-01-27 13:11:45 +0100

    reduce diff

--- scripts/mockdata-dist.sh
+++ scripts/mockdata-dist.sh
@@ -26,14 +26,7 @@ echo "[$1] Preparing mockdata (dist)"
 cd "$(dirname "$0")/.."
 cd tests/$1
 
-if [ -f .nvmrc ]; then
-    nvm use
-    echo "Using Node version: $(node -v)"
-else
-    echo "No .nvmrc file found, using default Node version from simple-frontend testdata"
-    nvm use $(cat ../simple-frontend/.nvmrc)
-fi
-
+nvm use
 echo "Using Node version: $(node -v)"
 echo "Using Package Manager: $(pm)"
 
