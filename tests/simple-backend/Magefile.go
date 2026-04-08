cb28f4172a2876f6573fac6d6d2f91189dbd63f8
Giuseppe Guerra <giuseppe@guerra.in>
2026-04-08 16:59:19 +0200

    add act tests

--- tests/simple-backend/Magefile.go
+++ tests/simple-backend/Magefile.go
@@ -4,9 +4,17 @@
 package main
 
 import (
+	"fmt"
+
 	// mage:import
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
 
 // Default configures the default target.
 var Default = build.BuildAll
+
+// BuildCustom is a custom build target used to test the backend-build-target CI input.
+func BuildCustom() {
+	fmt.Printf("::act-debug::msg=%q\n", "custom build target invoked")
+	build.BuildAll()
+}
