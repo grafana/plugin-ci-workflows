6d68f61b57f6c10d0c3d3d9237627c5dc7a2152d
Giuseppe Guerra <giuseppe@guerra.in>
2026-05-26 16:33:18 +0200

    ensure mock dist artifacts are unsigned

--- scripts/mockdata-dist-artifacts.sh
+++ scripts/mockdata-dist-artifacts.sh
@@ -13,6 +13,13 @@ cd "$(dirname "$0")/.."
 mkdir -p "tests/act/mockdata/dist-artifacts-unsigned/$1"
 
 echo "[$1] Packaging os/arch ZIPs"
+
+# Ensure that no signature token is set in order to produce unsigned ZIPs
+if [ -n "${GRAFANA_ACCESS_POLICY_TOKEN:-}" ]; then
+    echo "Warning: GRAFANA_ACCESS_POLICY_TOKEN is set, unsetting it to produce unsigned ZIPs"
+fi
+unset GRAFANA_ACCESS_POLICY_TOKEN
+
 # Will exit with 0 if the plugin has no backend
 # (in that case, there's no need for os/arch ZIPs, just universal)
 ./actions/internal/plugins/package/package.sh "tests/act/mockdata/dist/$1" "tests/act/mockdata/dist-artifacts-unsigned/$1"
